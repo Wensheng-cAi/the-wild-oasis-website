a80b48119432bc7aaaa6d1d137ef3af6df59970b
Wensheng-cAi <caiwensheng777@gmail.com>
2025-02-22 20:22:42 +0100

    display when no data in profile page

--- app/_components/UpdateProfilForm.js
+++ app/_components/UpdateProfilForm.js
@@ -50,7 +50,7 @@ function UpdateProfilForm({ children, guest }) {
                 <input
                     name="nationalID"
                     className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
-                    defaultValue={nationalID}
+                    defaultValue={nationalID ? nationalID : ''}
                 />
             </div>
 
